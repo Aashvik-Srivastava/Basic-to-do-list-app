855210d58efddeaadcdc6ccf9406fd7b18023728
Aashvik-Srivastava <aashviksrivastava@gmail.com>
2026-02-11 23:09:07 +0530

    Update to-do list.py

--- to-do list.py
+++ to-do list.py
@@ -24,3 +24,38 @@ while True:
         if new_task != "":
             tasks.append(new_task)
             print("Task added!")
+elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            count = 1
+            for task in tasks:
+                print(str(count) + ". " + task)
+                count += 1
+
+elif choice == "3":
+        number = input("Enter task number to remove: ")
+        if number.isdigit():
+            number = int(number)
+            if number >= 1 and number <= len(tasks):
+                removed = tasks.pop(number - 1)
+                print("Removed:", removed)
+            else:
+                print("Invalid task number.")
+        else:
+            print("Please enter a valid number.")
+
+  
+    elif choice == "4":
+        # Save tasks before exiting
+        with open(file_name, "w") as file:
+            for task in tasks:
+                file.write(task + "\n")
+        print("Tasks saved. Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Try again.")                
+
+
